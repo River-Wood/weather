bbacc31a1b10e5ac2d6f9bcdc7db562b0069e5cf
kinnon.wu <kinnon.wu@localhost.localdomain>
2017-06-08 09:38:26 +0800

    changes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 const API_KEY = 'c0c31e488297e0d029279f5cf8890844';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?origin=*&appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
-    const request = axios.get(url);
+    const request = axios({url,withCredentials: true});
 
     console.log('Request:', request);
     return {
